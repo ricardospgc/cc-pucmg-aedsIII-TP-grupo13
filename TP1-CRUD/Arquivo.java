425241f3eb98d28eef6896625dcc9144b5d049aa
yago-almeida-melo <yagoalmeidamelo27102004@gmail.com>
2024-09-12 17:20:00 -0300

    Added Comments and fix the deleting of BaseDeDados

--- TP1-CRUD/Arquivo.java
+++ TP1-CRUD/Arquivo.java
@@ -1,3 +1,4 @@
+
 import Hash.HashExtensivel;
 import Hash.ParIDEndereco;
 import java.io.File;
@@ -5,8 +6,8 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.reflect.Constructor;
 
-public class Arquivo<T extends Registro>{
-    
+public class Arquivo<T extends Registro> {
+
     final int CABECALHO = 4;
     Constructor<T> construtor;
     RandomAccessFile arquivo;
@@ -15,43 +16,60 @@ public class Arquivo<T extends Registro>{
     HashExtensivel<ParIDEndereco> indiceDireto;
 
     // Construtor
-    public Arquivo(Constructor<T> construtor, String nomeArquivo) throws Exception{
+    public Arquivo(Constructor<T> construtor, String nomeArquivo) throws Exception {
+        // Apagar a pasta "BaseDeDados" e todo o seu conteúdo antes de recriá-la
         File f = new File("BaseDeDados");
-        if(!f.exists()){
-            f.mkdir();
-        } 
-        new File(".//BaseDeDados//tarefas.db").delete();
-        
+        if (f.exists()) {
+            deleteDirectory(f);  // Método auxiliar para apagar a pasta e seus conteúdos
+        }
+        f.mkdir();  // Recria a pasta após apagar
+
         numRegistros = 0;
         this.construtor = construtor;
         this.nomeArquivo = ".//BaseDeDados//" + nomeArquivo;
         arquivo = new RandomAccessFile(this.nomeArquivo, "rw");
-        if(arquivo.length() < CABECALHO){
+        if (arquivo.length() < CABECALHO) {
             arquivo.seek(0);
             arquivo.writeInt(0);
         }
 
-        indiceDireto = new HashExtensivel<>(ParIDEndereco.class.getConstructor(), 4, this.nomeArquivo+".d.idx", this.nomeArquivo+".c.idx");
+        indiceDireto = new HashExtensivel<>(ParIDEndereco.class.getConstructor(), 4, this.nomeArquivo + ".d.idx", this.nomeArquivo + ".c.idx");
     }// Construtor
+
+    // Método auxiliar para apagar uma pasta e todo o seu conteúdo
+    private void deleteDirectory(File directory) {
+        File[] files = directory.listFiles();
+        if (files != null) { // Checa se não é um arquivo vazio ou não acessível
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteDirectory(file); // Chamada recursiva para subpastas
+                } else {
+                    file.delete(); // Deleta arquivos
+                }
+            }
+        }
+        directory.delete(); // Deleta a pasta vazia
+    }
 
     // ESTRUTURA DO ARQUIVO:
     /* ------------------------------------------------------ CABECALHO: int numRegistros */
-    /* ----------------------------------------------------------------------------- CRUD */
-    /* ------------------------------------------------------------ LAPIDE + TAMOBJ + OBJ */
-
+ /* ----------------------------------------------------------------------------- CRUD */
+ /* ------------------------------------------------------------ LAPIDE + TAMOBJ + OBJ */
     /**
-     * <p>Este metodo escreve um objeto no arquivo, atualizando o ID do objeto e 
-     * ajustando a posicao do ponteiro do arquivo conforme necessario. O metodo 
-     * tambem aumenta o valor do ID armazenado no inicio do arquivo antes de 
+     * <p>
+     * Este metodo escreve um objeto no arquivo, atualizando o ID do objeto e
+     * ajustando a posicao do ponteiro do arquivo conforme necessario. O metodo
+     * tambem aumenta o valor do ID armazenado no inicio do arquivo antes de
      * gravar o objeto.</p>
      *
-     * @param objeto O objeto a ser inserido no arquivo. 
+     * @param objeto O objeto a ser inserido no arquivo.
      * @return O ID do objeto inserido.
-     * @throws IOException Se ocorrer algum erro de I/O durante a gravacao do objeto no arquivo.
+     * @throws IOException Se ocorrer algum erro de I/O durante a gravacao do
+     * objeto no arquivo.
      */
-    protected int create(T objeto) throws Exception{
+    protected int create(T objeto) throws Exception {
         arquivo.seek(0);
-        int proximoId = arquivo.readInt()+1;    
+        int proximoId = arquivo.readInt() + 1;
         arquivo.seek(0);
         arquivo.writeInt(proximoId);
         numRegistros++;
@@ -69,17 +87,18 @@ public class Arquivo<T extends Registro>{
         return objeto.getId();
     }// create()
 
-
     /**
      * Metodo responsavel por ler um objeto do arquivo com base no seu ID.
-     * 
+     *
      * @param id O ID do objeto a ser lido.
      * @return O objeto correspondente ao ID, ou null se nao encontrado.
      * @throws IOException Se ocorrer um erro de I/O durante a leitura.
      */
     protected T read(int id) throws Exception {
-        if(id <= 0 || id > numRegistros) throw new Exception("Id invalido para leitura: " + id);
-        
+        if (id <= 0 || id > numRegistros) {
+            throw new Exception("Id invalido para leitura: " + id);
+        }
+
         byte[] b;
         byte lapide;
         T objeto;
@@ -87,8 +106,8 @@ public class Arquivo<T extends Registro>{
         arquivo.seek(CABECALHO);
 
         ParIDEndereco pid = indiceDireto.read(id);
-        
-        if(pid != null) {
+
+        if (pid != null) {
             arquivo.seek(pid.getEndereco());
             objeto = construtor.newInstance();
             lapide = arquivo.readByte();
@@ -96,53 +115,54 @@ public class Arquivo<T extends Registro>{
             b = new byte[tamRegistro];
             arquivo.read(b);
 
-            if(lapide == ' '){
+            if (lapide == ' ') {
                 objeto.fromByteArray(b);
-                    if(objeto.getId() == id) return objeto;
+                if (objeto.getId() == id) {
+                    return objeto;
+                }
             }
-        } 
+        }
 
-        return null;  
+        return null;
     }// read()
 
     /**
      * Metodo responsavel por atualizar um objeto do arquivo com base no seu ID.
-     * 
+     *
      * @param id O ID do objeto a ser atualizado.
-     * @param T Objeto com os dados 
+     * @param T Objeto com os dados
      * @return boolean indicando se foi ou nao atualizado.
      * @throws IOException Se ocorrer um erro de I/O durante a atualizacao.
      */
-    protected boolean update(T novoObjeto) throws Exception{
+    protected boolean update(T novoObjeto) throws Exception {
         byte[] b;
         byte lapide;
         T objeto;
         short tamRegistro;
-        
+
         ParIDEndereco pie = indiceDireto.read(novoObjeto.getId());
 
         /* quando o update atualizar o status de um objeto para concluido, 
         ele deve atualizar a data de conclusao pro momento do update */
-        if(pie!=null){
+        if (pie != null) {
             arquivo.seek(pie.getEndereco());
             objeto = construtor.newInstance();
             lapide = arquivo.readByte();
 
-            if(lapide == ' '){
+            if (lapide == ' ') {
                 tamRegistro = arquivo.readShort();
                 b = new byte[tamRegistro];
                 arquivo.read(b);
                 objeto.fromByteArray(b);
-                
-                if(objeto.getId() == novoObjeto.getId()){
+
+                if (objeto.getId() == novoObjeto.getId()) {
                     byte[] bb = novoObjeto.toByteArray();
                     short tamNovoRegistro = (short) bb.length;
 
-                    if(tamNovoRegistro <= tamRegistro){
-                        arquivo.seek(pie.getEndereco()+3);
+                    if (tamNovoRegistro <= tamRegistro) {
+                        arquivo.seek(pie.getEndereco() + 3);
                         arquivo.write(bb);
-                    }
-                    else{
+                    } else {
                         arquivo.seek(pie.getEndereco());
                         arquivo.writeByte('*');
                         arquivo.seek(arquivo.length());
@@ -154,33 +174,35 @@ public class Arquivo<T extends Registro>{
                     }
                     return true;
                 }
-            }   
+            }
         }
         return false;
     }// update
 
-
     /**
      * Metodo responsavel por deletar um objeto do arquivo com base no seu ID.
-     * 
+     *
      * @param id O ID do objeto a ser deletado.
      * @return boolean indicando se foi ou nao deletado.
-     * @throws IOException Se ocorrer um erro de I/O durante o processo de delete.
+     * @throws IOException Se ocorrer um erro de I/O durante o processo de
+     * delete.
      */
-    protected boolean delete(int id) throws Exception{
-        if(id <= 0 || id > numRegistros) throw new Exception("Id invalido para leitura: " + id);
+    protected boolean delete(int id) throws Exception {
+        if (id <= 0 || id > numRegistros) {
+            throw new Exception("Id invalido para leitura: " + id);
+        }
 
         boolean fim = false;
-        
+
         byte[] b;
         byte lapide;
         Long endereco;
         T objeto;
         short tamRegistro;
-        
+
         arquivo.seek(CABECALHO);
 
-        while(arquivo.getFilePointer() < arquivo.length() || !fim){
+        while (arquivo.getFilePointer() < arquivo.length() || !fim) {
             endereco = arquivo.getFilePointer();
             lapide = arquivo.readByte();
             objeto = construtor.newInstance();
@@ -188,17 +210,17 @@ public class Arquivo<T extends Registro>{
             b = new byte[tamRegistro];
             arquivo.read(b);
 
-            if(lapide == ' '){
+            if (lapide == ' ') {
                 objeto.fromByteArray(b);
-                if(objeto.getId() == id){
+                if (objeto.getId() == id) {
                     arquivo.seek(endereco);
                     arquivo.write('*');
-                    fim = true; 
+                    fim = true;
                 }
             }
         }
         return fim;
     }// delete()
 
-    /* ------------------------------------------------------------------------------------ */ 
+    /* ------------------------------------------------------------------------------------ */
 }
--- TP1-CRUD/Principal.java
+++ TP1-CRUD/Principal.java
@@ -1,43 +1,63 @@
+
 import Enums.Prioridade;
 import Enums.Status;
 
 public class Principal {
+
     public static void main(String[] args) {
-    try {
-        
-        Tarefa tf1 = new Tarefa("teste", Status.PENDENTE, Prioridade.BAIXA);
-        Tarefa tf2 = new Tarefa("teste2", Status.CONCLUIDO, Prioridade.ALTA);
-
-        Arquivo<Tarefa> arqTarefa = new Arquivo<>(Tarefa.class.getConstructor(), "tarefas.db");
-        
-        int id1 = arqTarefa.create(tf1);
-        System.out.println(arqTarefa.read(id1));
-
-        int id2 = arqTarefa.create(tf2);
-        System.out.println(arqTarefa.read(id2));
-
-        System.out.println("\n-------");
-
-        tf1.setNome("testando"); tf1.setPrioridade(Prioridade.URGENTE); tf1.setStatus(Status.EM_PROGRESSO);
-        boolean hasUpdated = arqTarefa.update(tf1);
-
-        if(hasUpdated){
-            System.out.println("\nUpdate bem sucedido!");
+        try {
+            //Criando os Objetos de Tarefa...
+            Tarefa tf1 = new Tarefa("Trabalho Interdisciplinar", Status.CONCLUIDO, Prioridade.BAIXA);
+            Tarefa tf2 = new Tarefa("TP 1000 - AEDS III", Status.PENDENTE, Prioridade.URGENTE);
+            Tarefa tf3 = new Tarefa("Estudar Cálculo II", Status.EM_PROGRESSO, Prioridade.ALTA);
+            //Abrindo o arquivo de tarefas
+            Arquivo<Tarefa> arqTarefa = new Arquivo<>(Tarefa.class.getConstructor(), "tarefas.db");
+            //Inserção dos registros 
+            int id1 = arqTarefa.create(tf1);
+            int id2 = arqTarefa.create(tf2);
+            int id3 = arqTarefa.create(tf3);
+            //Leitura de todos os registros 
             System.out.println(arqTarefa.read(id1));
-        } else {
-            System.out.println("Erro ao atualizar");
-        }
+            System.out.println(arqTarefa.read(id2));
+            System.out.println(arqTarefa.read(id3));
 
-        boolean hasDeleted = arqTarefa.delete(id2);
-        if(hasDeleted){
-            System.out.println("\nDelete bem sucedido!");
-        } else {
-            System.out.println("Erro ao deletar");
-        }
+            System.out.println("\n-------");
+            //Atualiza uma tarefa para um tamanho menor e exibe o resultado
+            tf1.setNome("Prova: Banco de Dados");
+            tf1.setPrioridade(Prioridade.URGENTE);
+            tf1.setStatus(Status.EM_PROGRESSO);
+            boolean hasUpdated = arqTarefa.update(tf1);
+            if (hasUpdated) {
+                System.out.println("\nUpdate bem sucedido na Tarefa de Id " + id1 + "!");
+                System.out.println(arqTarefa.read(id1));
+            } else {
+                System.out.println("Erro ao atualizar");
+            }
 
-            
-    } catch(Exception e) {
-        e.getMessage();
-    }
+            System.out.println("\n-------");
+            //Atualiza uma tarefa para um tamanho maior e exibe o resultado
+            tf3.setNome("Lista de Arquitetura de Computadores");
+            tf3.setPrioridade(Prioridade.ALTA);
+            tf3.setStatus(Status.CONCLUIDO);
+            hasUpdated = arqTarefa.update(tf3);
+            if (hasUpdated) {
+                System.out.println("\nUpdate bem sucedido na Tarefa de Id " + id3 + "!");
+                System.out.println(arqTarefa.read(id3));
+            } else {
+                System.out.println("Erro ao atualizar");
+            }
+
+            System.out.println("\n-------");
+            //Operação de deletar uma Tarefa
+            boolean hasDeleted = arqTarefa.delete(id2);
+            if (hasDeleted) {
+                System.out.println("\nDelete bem sucedido!");
+            } else {
+                System.out.println("Erro ao deletar");
+            }
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }// Principal()
